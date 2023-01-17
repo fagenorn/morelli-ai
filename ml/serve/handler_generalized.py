364f9b93e4fa4f1b710a7f6de673d2495811cfc9
fagenorn <anisanissakkaf@gmail.com>
2023-01-17 16:37:32 +0100

    torchserve handler

---
+++ ml/serve/handler_generalized.py
@@ -0,0 +1,337 @@
+import ast
+import base64
+import io
+import json
+import logging
+import os
+from abc import ABC
+from PIL import Image
+import torch
+import transformers
+# from captum.attr import LayerIntegratedGradients
+from transformers import (
+    AutoModelForImageClassification,
+    AutoFeatureExtractor,
+)
+from torchvision.transforms import (CenterCrop, Compose, Normalize,
+                                    Resize, ToTensor)
+from ts.torch_handler.base_handler import BaseHandler
+
+logger = logging.getLogger(__name__)
+logger.info("Transformers version %s", transformers.__version__)
+
+
+class TransformersSeqClassifierHandler(BaseHandler, ABC):
+    """
+    Transformers handler class for sequence, token classification and question answering.
+    """
+
+    def __init__(self):
+        super(TransformersSeqClassifierHandler, self).__init__()
+        self.initialized = False
+
+    def initialize(self, ctx):
+        """In this initialize function, the BERT model is loaded and
+        the Layer Integrated Gradients Algorithm for Captum Explanations
+        is initialized here.
+        Args:
+            ctx (context): It is a JSON Object containing information
+            pertaining to the model artefacts parameters.
+        """
+        self.manifest = ctx.manifest
+        properties = ctx.system_properties
+        model_dir = properties.get("model_dir")
+        serialized_file = self.manifest["model"]["serializedFile"]
+        model_pt_path = os.path.join(model_dir, serialized_file)
+
+        self.device = torch.device(
+            "cuda:" + str(properties.get("gpu_id"))
+            if torch.cuda.is_available() and properties.get("gpu_id") is not None
+            else "cpu"
+        )
+        # read configs for the mode, model_name, etc. from setup_config.json
+        setup_config_path = os.path.join(model_dir, "setup_config.json")
+        if os.path.isfile(setup_config_path):
+            with open(setup_config_path) as setup_config_file:
+                self.setup_config = json.load(setup_config_file)
+        else:
+            logger.warning("Missing the setup_config.json file.")
+
+        # Loading the shared object of compiled Faster Transformer Library if Faster Transformer is set
+        if self.setup_config["FasterTransformer"]:
+            faster_transformer_complied_path = os.path.join(
+                model_dir, "libpyt_fastertransformer.so"
+            )
+            torch.classes.load_library(faster_transformer_complied_path)
+        # Loading the model and tokenizer from checkpoint and config files based on the user's choice of mode
+        # further setup config can be added.
+        if self.setup_config["save_mode"] == "torchscript":
+            self.model = torch.jit.load(model_pt_path, map_location=self.device)
+        elif self.setup_config["save_mode"] == "pretrained":
+            if self.setup_config["mode"] == "text_generation":
+                self.model = AutoModelForImageClassification.from_pretrained(model_dir)
+            else:
+                logger.warning("Missing the operation mode.")
+            # Using the Better Transformer integration to speedup the inference
+            if self.setup_config["BetterTransformer"]:
+                try:
+                    from optimum.bettertransformer import BetterTransformer
+
+                    self.model = BetterTransformer.transform(self.model)
+                except ImportError as error:
+                    logger.warning(
+                        "HuggingFace Optimum is not installed. Proceeding without BetterTransformer"
+                    )
+                except RuntimeError as error:
+                    logger.warning(
+                        "HuggingFace Optimum is not supporting this model,for the list of supported models, please refer to this doc,https://huggingface.co/docs/optimum/bettertransformer/overview"
+                    )
+
+                self.model.to(self.device)
+
+        else:
+            logger.warning("Missing the checkpoint or state_dict.")
+
+
+
+        self.feature_extractor = AutoFeatureExtractor.from_pretrained(
+            self.setup_config["model_name"],
+        )
+
+        if "shortest_edge" in self.feature_extractor.size:
+            size = self.feature_extractor.size["shortest_edge"]
+        else:
+            size = (self.feature_extractor.size["height"], self.feature_extractor.size["width"])
+
+        normalize = Normalize(mean=self.feature_extractor.image_mean, std=self.feature_extractor.image_std)
+        _val_transforms = Compose(
+            [
+                Resize(size),
+                CenterCrop(size),
+                ToTensor(),
+                normalize,
+            ]
+        )
+
+        def val_transforms(image):
+            """Apply _val_transforms across a batch."""
+            image = _val_transforms(image.convert("RGB"))
+            return image
+        
+        self.image_processing = val_transforms
+
+        self.model.eval()
+        logger.info("Transformer model from path %s loaded successfully", model_dir)
+
+        self.initialized = True
+
+    def preprocess(self, requests):
+        """The preprocess function of MNIST program converts the input data to a float tensor
+        Args:
+            data (List): Input data from the request is in the form of a Tensor
+        Returns:
+            list : The preprocess function returns the input image as a list of float tensors.
+        """
+        images = []
+        for idx, data in enumerate(requests):
+            image = data.get("data") or data.get("body")
+            if isinstance(image, str):
+                # if the image is a string of bytesarray.
+                image = base64.b64decode(image)
+
+            # If the image is sent as bytesarray
+            if isinstance(image, (bytearray, bytes)):
+                image = Image.open(io.BytesIO(image))
+                image = self.image_processing(image)
+            else:
+                # if the image is a list
+                image = torch.FloatTensor(image)
+
+            images.append(image)
+
+        return torch.stack(images).to(self.device)
+
+    def inference(self, data):
+        """Predict the class (or classes) of the received text using the
+        serialized transformers checkpoint.
+        Args:
+            input_batch (list): List of Text Tensors from the pre-process function is passed here
+        Returns:
+            list : It returns a list of the predicted value for the input text
+        """
+
+        with torch.no_grad():
+            inputs = self.feature_extractor(images=data, return_tensors="pt")
+            marshalled_data = inputs.to(self.device)
+            results = self.model(**marshalled_data)
+
+        return results
+
+    def postprocess(self, inference_output):
+        """Post Process Function converts the predicted response into Torchserve readable format.
+        Args:
+            inference_output (list): It contains the predicted response of the input text.
+        Returns:
+            (list): Returns a list of the Predictions and Explanations.
+        """
+        return (inference_output.logits, inference_output.attentions)
+
+    def get_insights(self, input_batch, text, target):
+        """This function initialize and calls the layer integrated gradient to get word importance
+        of the input text if captum explanation has been selected through setup_config
+        Args:
+            input_batch (int): Batches of tokens IDs of text
+            text (str): The Text specified in the input request
+            target (int): The Target can be set to any acceptable label under the user's discretion.
+        Returns:
+            (list): Returns a list of importances and words.
+        """
+
+        return []
+        if self.setup_config["captum_explanation"]:
+            embedding_layer = getattr(self.model, self.setup_config["embedding_name"])
+            embeddings = embedding_layer.embeddings
+            self.lig = LayerIntegratedGradients(captum_sequence_forward, embeddings)
+        else:
+            logger.warning("Captum Explanation is not chosen and will not be available")
+
+        if isinstance(text, (bytes, bytearray)):
+            text = text.decode("utf-8")
+        text_target = ast.literal_eval(text)
+
+        if not self.setup_config["mode"] == "question_answering":
+            text = text_target["text"]
+        self.target = text_target["target"]
+
+        input_ids, ref_input_ids, attention_mask = construct_input_ref(
+            text, self.tokenizer, self.device, self.setup_config["mode"]
+        )
+        all_tokens = get_word_token(input_ids, self.tokenizer)
+        response = {}
+        response["words"] = all_tokens
+        if (
+            self.setup_config["mode"] == "sequence_classification"
+            or self.setup_config["mode"] == "token_classification"
+        ):
+
+            attributions, delta = self.lig.attribute(
+                inputs=input_ids,
+                baselines=ref_input_ids,
+                target=self.target,
+                additional_forward_args=(attention_mask, 0, self.model),
+                return_convergence_delta=True,
+            )
+
+            attributions_sum = summarize_attributions(attributions)
+            response["importances"] = attributions_sum.tolist()
+            response["delta"] = delta[0].tolist()
+
+        elif self.setup_config["mode"] == "question_answering":
+            attributions_start, delta_start = self.lig.attribute(
+                inputs=input_ids,
+                baselines=ref_input_ids,
+                target=self.target,
+                additional_forward_args=(attention_mask, 0, self.model),
+                return_convergence_delta=True,
+            )
+            attributions_end, delta_end = self.lig.attribute(
+                inputs=input_ids,
+                baselines=ref_input_ids,
+                target=self.target,
+                additional_forward_args=(attention_mask, 1, self.model),
+                return_convergence_delta=True,
+            )
+            attributions_sum_start = summarize_attributions(attributions_start)
+            attributions_sum_end = summarize_attributions(attributions_end)
+            response["importances_answer_start"] = attributions_sum_start.tolist()
+            response["importances_answer_end"] = attributions_sum_end.tolist()
+            response["delta_start"] = delta_start[0].tolist()
+            response["delta_end"] = delta_end[0].tolist()
+
+        return [response]
+
+
+def construct_input_ref(text, tokenizer, device, mode):
+    """For a given text, this function creates token id, reference id and
+    attention mask based on encode which is faster for captum insights
+    Args:
+        text (str): The text specified in the input request
+        tokenizer (AutoTokenizer Class Object): To word tokenize the input text
+        device (cpu or gpu): Type of the Environment the server runs on.
+    Returns:
+        input_id(Tensor): It attributes to the tensor of the input tokenized words
+        ref_input_ids(Tensor): Ref Input IDs are used as baseline for the attributions
+        attention mask() :  The attention mask is a binary tensor indicating the position
+         of the padded indices so that the model does not attend to them.
+    """
+    if mode == "question_answering":
+        question_context = ast.literal_eval(text)
+        question = question_context["question"]
+        context = question_context["context"]
+        text_ids = tokenizer.encode(question, context, add_special_tokens=False)
+
+    text_ids = tokenizer.encode(text, add_special_tokens=False)
+    # construct input token ids
+    logger.info("text_ids %s", text_ids)
+    logger.info("[tokenizer.cls_token_id] %s", [tokenizer.cls_token_id])
+    input_ids = [tokenizer.cls_token_id] + text_ids + [tokenizer.sep_token_id]
+    logger.info("input_ids %s", input_ids)
+
+    input_ids = torch.tensor([input_ids], device=device)
+    # construct reference token ids
+    ref_input_ids = (
+        [tokenizer.cls_token_id]
+        + [tokenizer.pad_token_id] * len(text_ids)
+        + [tokenizer.sep_token_id]
+    )
+    ref_input_ids = torch.tensor([ref_input_ids], device=device)
+    # construct attention mask
+    attention_mask = torch.ones_like(input_ids)
+    return input_ids, ref_input_ids, attention_mask
+
+
+def captum_sequence_forward(inputs, attention_mask=None, position=0, model=None):
+    """This function is used to get the predictions from the model and this function
+    can be used independent of the type of the BERT Task.
+    Args:
+        inputs (list): Input for Predictions
+        attention_mask (list, optional): The attention mask is a binary tensor indicating the position
+         of the padded indices so that the model does not attend to them, it defaults to None.
+        position (int, optional): Position depends on the BERT Task.
+        model ([type], optional): Name of the model, it defaults to None.
+    Returns:
+        list: Prediction Outcome
+    """
+    model.eval()
+    model.zero_grad()
+    pred = model(inputs, attention_mask=attention_mask)
+    pred = pred[position]
+    return pred
+
+
+def summarize_attributions(attributions):
+    """Summarises the attribution across multiple runs
+    Args:
+        attributions ([list): attributions from the Layer Integrated Gradients
+    Returns:
+        list : Returns the attributions after normalizing them.
+    """
+    attributions = attributions.sum(dim=-1).squeeze(0)
+    attributions = attributions / torch.norm(attributions)
+    return attributions
+
+
+def get_word_token(input_ids, tokenizer):
+    """constructs word tokens from token id using the BERT's
+    Auto Tokenizer
+    Args:
+        input_ids (list): Input IDs from construct_input_ref method
+        tokenizer (class): The Auto Tokenizer Pre-Trained model object
+    Returns:
+        (list): Returns the word tokens
+    """
+    indices = input_ids[0].detach().tolist()
+    tokens = tokenizer.convert_ids_to_tokens(indices)
+    # Remove unicode space character from BPE Tokeniser
+    tokens = [token.replace("Ġ", "") for token in tokens]
+    return tokens
