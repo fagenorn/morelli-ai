ad267e0b09fe1db46b1eb991a687415f3c772429
fagenorn <anisanissakkaf@gmail.com>
2023-01-18 14:26:31 +0100

    Simple django server

---
+++ apps/detector/static/js/script.js
@@ -0,0 +1,61 @@
+loading = false;
+
+function handleDrop(event) {
+    event.preventDefault();
+
+    if (loading)
+        return;
+
+    loading = true;
+    var files = event.dataTransfer.files;
+    loadFile(files[0]);
+}
+
+function loadFile(file) {
+    document.querySelector('.loading').classList.add('is-visible');
+    document.querySelector('#output').classList.add('is-loading');
+
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(file);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.querySelector('.loading').classList.remove('is-visible');
+            document.querySelector('#output').classList.remove('is-loading');
+
+            var response = JSON.parse(xhr.responseText);
+            var responseDiv = document.getElementById("response");
+            responseDiv.classList.remove("success", "error");
+
+            // var output = document.getElementById('output');
+            // output.src = "data:image/png;base64," + response.heatmap;
+
+            var percentage = (1 - response.ai_chance) * 100;
+            document.querySelector('#percentage').innerHTML = percentage.toFixed(2) + '%' + " Human";
+            document.querySelector('#progress').style.width = percentage + '%';
+            if (percentage >= 50) {
+                document.querySelector('#progress').style.backgroundColor = '#4CAF50';
+            } else {
+                document.querySelector('#progress').style.backgroundColor = '#F44336';
+            }
+
+            if (response.label === "human") {
+                responseDiv.innerHTML = "HUMAN";
+                responseDiv.classList.add("success");
+            } else if (response.label === "ai") {
+                responseDiv.innerHTML = "AI";
+                responseDiv.classList.add("error");
+            } else {
+                responseDiv.innerHTML = response.label;
+                responseDiv.classList.add("error");
+            }
+        }
+
+        loading = false;
+    };
+
+    var formData = new FormData();
+    formData.append("file", file);
+    xhr.send(formData);
+}
--- morelli/settings.py
+++ morelli/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local apps
+    "apps.detector",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = "morelli.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
