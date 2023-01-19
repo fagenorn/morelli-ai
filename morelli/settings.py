4dba0c76d07d9688233589cd27584fb158121b70
fagenorn <anisanissakkaf@gmail.com>
2023-01-19 11:47:03 +0100

    Update models config

--- morelli/settings.py
+++ morelli/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-qvzyzr=^oagi1t!$nu0ttgbwqy+of$zm$uqlqe_ob6qp31=r9+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ "*" ]
 
