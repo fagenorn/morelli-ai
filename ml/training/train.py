a5911dbcb088e807160b878572ed1672f1b3af63
fagenorn <anisanissakkaf@gmail.com>
2023-01-19 13:42:26 +0100

    Move to device

--- ml/training/train.py
+++ ml/training/train.py
@@ -204,7 +204,7 @@ class CustomTrainer(Trainer):
         outputs = model(**inputs)
         logits = outputs.get("logits")
         # compute custom loss (suppose one has 3 labels with different weights)
-        loss_fct = torch.nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0]))
+        loss_fct = torch.nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0])).to(self.args.device)
         loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
         return (loss, outputs) if return_outputs else loss
     
