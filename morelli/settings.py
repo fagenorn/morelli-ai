27592cdd431a59da32438c0eae86f67487a49e0e
fagenorn <anisanissakkaf@gmail.com>
2023-01-19 12:20:45 +0100

    Added CORS

--- morelli/settings.py
+++ morelli/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [ "*" ]
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders"
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -125,3 +127,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_ROOT = "/var/www/morelli.ai/static/"
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://morelli.ai",
+]
